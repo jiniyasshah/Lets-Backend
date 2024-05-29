9db55494a7936080f4ea35afe48ffcb6f3e13403
sanks <jiniyasshah>
2024-05-29 17:58:12 +0545

    Implemented connectDB Function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`Error: ${error}`);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running at port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MongoDB connection FAILED !! ${error}`);
+  });
