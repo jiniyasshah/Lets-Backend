55948097a4568f259cb1cdccb20a9776885cf7cb
sanks <jiniyasshah>
2024-06-01 23:02:05 +0545

    Connected to Database with Error handling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 import { app } from "./app.js";
 
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./.env" });
 const port = process.env.PORT || 3000;
 
 connectDB()
